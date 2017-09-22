8724ac642f894817d6867e7ebcb1343d71627326
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-22 15:37:33 -0400

    Make paperboy deliver quota of papers to display earnings

--- paperboy/paperboy-class.rb
+++ paperboy/paperboy-class.rb
@@ -6,9 +6,19 @@ class Paperboy
     @earnings = 0
   end
 
+  def quota
+    50 + (@experience / 2)
+  end
+
   def deliver(start_address, end_address)
     houses_delivered = end_address - start_address
-    @experience = houses_delivered / 2
+    if houses_delivered == quota
+      money = quota * 0.25
+      @earnings += money
+    
+    end
+    @experience += houses_delivered
+    @earnings
   end
 
 end
--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -1,1 +1,6 @@
 require "./paperboy-class.rb"
+
+tommy = Paperboy.new("Tommy")
+
+puts tommy.quota
+puts tommy.deliver(1, 51)
