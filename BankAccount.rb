60f314a163f04909b6fbfed825ac8734d72082eb
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-22 13:40:59 -0400

    Create instance method deposit

--- BankAccount.rb
+++ BankAccount.rb
@@ -1,8 +1,12 @@
 class BankAccount
 
-  def initialize()
-    @balance
-    @interest_rate
+  def initialize(balance, interest_rate)
+    @balance = balance
+    @interest_rate = interest_rate
+  end
+
+  def depoist(money_in)
+    @balance + money_in
   end
 
 end
---
+++ object-oriented-programming-classes-and-objects.rb
@@ -0,0 +1,5 @@
+require "./BankAccount.rb"
+
+account1 = BankAccount.new(100.00, 1.0)
+
+puts account1.depoist(50)
