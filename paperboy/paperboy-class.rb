2bcea20c06042423016e9b09d16d42fd29298c0f
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-22 16:14:49 -0400

    Experience returns correctly

--- paperboy/paperboy-class.rb
+++ paperboy/paperboy-class.rb
@@ -16,24 +16,33 @@ class Paperboy
 
   def deliver(start_address, end_address)
     houses_delivered = end_address - start_address
+
     if houses_delivered == quota
       money = quota * 0.25
+      extra_money = 0
       @earnings += money
+
     elsif houses_delivered > quota
       money = quota * 0.25
       @earnings += money
       extra_houses = houses_delivered - quota
       extra_money = extra_houses * 0.5
       @earnings += extra_money
+
     else
       @earnings -= 2
-      return -2
+      money = -2
+      extra_money = 0
     end
+
     @experience += houses_delivered
     daily_wage = money + extra_money
+
   end
 
 
-
+  def report
+    "I'm #{ @name }, and I've delivered #{ @experience } papers and I've earned $#{ @earnings } so far!"
+  end
 
 end
--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -5,3 +5,9 @@ tommy = Paperboy.new("Tommy")
 puts tommy.quota
 puts tommy.deliver(101, 160)
 puts tommy.earnings
+puts tommy.report
+
+puts tommy.quota
+puts tommy.deliver(1, 75)
+puts tommy.earnings
+puts tommy.report
