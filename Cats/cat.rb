44947c6e62e3fc653701b9f71402d299c7e7d4b4
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-22 14:10:23 -0400

    Create instance method meal_time

--- Cats/cat.rb
+++ Cats/cat.rb
@@ -4,7 +4,14 @@ class Cat
     @name = name
     @preferred_food = preferred_food
     @meal_time = meal_time
+  end
 
+  def meal_time
+    if @meal_time < 12
+      "#{ @meal_time }AM"
+    else
+      "#{@meal_time - 12}PM"
+    end
   end
 
 end
--- Cats/catting.rb
+++ Cats/catting.rb
@@ -2,3 +2,6 @@ require "./cat.rb"
 
 vince = Cat.new("Vince", "tuna", 18)
 mishka = Cat.new("Mishka", "mice", 8)
+
+puts vince.meal_time
+puts mishka.meal_time
